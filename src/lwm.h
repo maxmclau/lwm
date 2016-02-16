ee5b085ec5b686870b6bddc923dfd9515460b5fd
Maxmillion McLaughlin <github@maxmclau.com>
2016-02-16 10:14:55 -0500

    Initial commit and restructure

--- src/lwm.h
+++
@@ -1,6 +0,0 @@
-// Dummy file that can be included by sketches to let the Arduino IDE
-// autodetect the dependency on this library. It will then add the src/
-// directory to the include path, so sketches can include the specific
-// files they need. For example:
-// #include <lwm.h>
-// #include <lwm/nwk/nwk.h>
